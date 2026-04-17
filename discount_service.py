482df54ea2539c926fb38e595951178c02461d6e
kru45 <72514792+kru45@users.noreply.github.com>
2026-04-17 22:01:59 +0530

    Create discount_service.py

---
+++ discount_service.py
@@ -0,0 +1,13 @@
+import os
+
+def apply_discount(price, code_provided):
+    # Fetch the "Official" code from Jenkins environment
+    secret_code = os.getenv('DISCOUNT_CODE')
+    
+    if code_provided == secret_code:
+        return price * 0.80  # 20% off
+    return price
+
+if __name__ == "__main__":
+    # Test it locally with a sample price
+    print(f"Final Price for $100: {apply_discount(100, 'SAVE20')}")
